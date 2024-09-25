3f496bc5aa01d8f4fc3c318c24a9249ba0cfb5a0
Kaique-GM <kaiquegmoficial@gmail.com>
2024-09-25 00:47:25 -0300

    Update implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -32,10 +32,17 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Test 4: Seller findAll ===");
+		System.out.println("\n=== Test 4: Seller insert ===");
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+	
+		System.out.println("\n=== Test 5: Seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
